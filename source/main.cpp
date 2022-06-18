0ed50a3efa2a0105f5f3255c068cce9a1d4c9631
Mateo Gonzales Navarrete <mgonzalesn17@outlook.com>
2022-06-18 02:42:53 -0500

    Modify main to write binary dataset

--- source/main.cpp
+++ source/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <sstream>
 
 struct BinaryHeader {
   uint32_t bytes;
@@ -9,11 +10,17 @@ struct BinaryHeader {
 
 struct coordinates_t {
   float lat, lon;
+
+  coordinates_t(float lat, float lon) {
+    this->lat = lat;
+    this->lon = lon;
+  }
 };
 
 BinaryHeader header;
 
-std::string path = "../data/brightkite-example.nds";
+std::string path = "../data/brightkite.nds";
+std::string write_path = "../data/brightkite_with_classes_ni.nds";
 
 template <typename T>
 void get_data(std::vector<uint8_t>& data, uint8_t offset) {
@@ -34,13 +41,77 @@ T* get_entry(std::vector<uint8_t>& data, size_t id) {
   return ((T*)&data[id * sizeof(T)]);
 }
 
-
 int main() {
-  std::ifstream infile(path, std::ios::binary);
+  std::ifstream infile("../data/brightkite_with_classes_ni.csv");
 
-  infile.read((char*)&header, sizeof(BinaryHeader));
+  std::vector<uint8_t> days;
+  std::vector<uint8_t> hrs;
+  std::vector<uint32_t> timestamps;
+  std::vector<coordinates_t> coords;
+  std::vector<uint8_t> classes;
+  std::vector<float> color_classes;
 
-  infile.close();
+  std::string line, word;
+
+  getline(infile, line);
+
+  while (getline(infile, line)) {
+    std::stringstream str(line);
+    // std::cout << str.str() << std::endl;
+
+    getline(str, word, ',');
+    days.push_back(std::stoi(word));
+
+    getline(str, word, ',');
+    hrs.push_back(std::stoi(word));
+
+    getline(str, word, ',');
+    timestamps.push_back(std::stoi(word));
+
+    getline(str, word, ',');
+    float lat = std::stof(word);
+    getline(str, word, ',');
+    float lon = std::stof(word);
+    coords.push_back(coordinates_t(lat, lon));
+
+    getline(str, word, ',');
+    classes.push_back(std::stoi(word));
+
+    getline(str, word);
+    color_classes.push_back(std::stof(word));
+  }
+
+  for (size_t i = 0; i < 10; ++i)
+    std::cout << +days[i] << " " << +hrs[i] << " " << timestamps[i] << " " <<
+              coords[i].lat << " " << coords[i].lon << " " << +classes[i] << " " <<
+              color_classes[i] << "\n";
+
+  std::ofstream outFile("../data/brightkite_with_classes_ni.nds",
+                        std::ios::binary);
+
+  std::ifstream infile3("../data/brightkite.nds", std::ios::binary);
+
+  infile3.read((char*)&header, sizeof(BinaryHeader));
+
+  infile3.close();
+
+  outFile.write((char*)&header, sizeof(BinaryHeader));
+  outFile.write((char*)&coords[0], sizeof(coordinates_t) * days.size());
+  outFile.write((char*)&days[0], sizeof(uint8_t) * days.size());
+  outFile.write((char*)&hrs[0], sizeof(uint8_t) * days.size());
+  outFile.write((char*)&timestamps[0], sizeof(uint32_t) * days.size());
+  // outFile.write(reinterpret_cast<char*>(&classes[0]), sizeof(uint8_t) * days.size());
+  // outFile.write(reinterpret_cast<char*>(&color_classes[0]), sizeof(float) * days.size());
+  outFile.close();
+
+  std::cout << "Reading..." << std::endl;
+
+  std::ifstream infile2("../data/brightkite_with_classes_ni.nds",
+                        std::ios::binary);
+
+  infile2.read((char*)&header, sizeof(BinaryHeader));
+
+  infile2.close();
 
   std::cout << "Total number of records: " << header.records << "\n";
 
@@ -52,12 +123,12 @@ int main() {
   uint8_t offset = 8;
   get_data<categorical_t>(data, offset);
 
-  for (int i = 0; i < header.records; ++i) {
-    uint32_t entry = (*get_entry<categorical_t>(data, i));
-    std::cout << entry << ";";
-  }
-  std::cout << std::endl;
-   
+  // for (size_t i = 0; i < header.records; ++i) {
+  // uint32_t entry = (*get_entry<categorical_t>(data, i));
+  // std::cout << entry << ";";
+  // }
+  // std::cout << std::endl;
+
 
   // Read hour_of_day
   std::vector<uint8_t> data2;
@@ -65,12 +136,12 @@ int main() {
   offset = 9;
   get_data<categorical_t>(data2, offset);
 
-  for (int i = 0; i < header.records; ++i) {
-    uint32_t entry = (*get_entry<categorical_t>(data2, i));
-    std::cout << entry << ";";
-  }
-  std::cout << std::endl;
-  
+  // for (size_t i = 0; i < header.records; ++i) {
+  // uint32_t entry = (*get_entry<categorical_t>(data2, i));
+  // std::cout << entry << ";";
+  // }
+  // std::cout << std::endl;
+
 
   // Read temporal
   std::vector<uint8_t> data3;
@@ -78,20 +149,35 @@ int main() {
   offset = 10;
   get_data<temporal_t>(data3, offset);
 
-  for (int i = 0; i < header.records; ++i) {
-    uint32_t entry = (*get_entry<temporal_t>(data3, i));
-    uint32_t value = static_cast<temporal_t>(entry / static_cast<float>(604800)) * 604800;
-    std::cout << value << ";";
-  }
+  // for (size_t i = 0; i < header.records; ++i) {
+  // uint32_t entry = (*get_entry<temporal_t>(data3, i));
+  // uint32_t value = static_cast<temporal_t>(entry / static_cast<float>(604800)) * 604800;
+  // std::cout << value << ";";
+  // }
 
   // Read spatial
   std::vector<uint8_t> data4;
-  offset = 32;
+  offset = 0;
   get_data<coordinates_t>(data4, offset);
 
-  for (int i = 0; i < header.records; ++i) {
-    coordinates_t entry = (*get_entry<coordinates_t>(data, i));
-    std::cout << entry.lat << std::endl;
+  // for (size_t i = 0; i < header.records; ++i) {
+  // coordinates_t entry = (*get_entry<coordinates_t>(data4, i));
+  // std::cout << entry.lat << std::endl;
+  // }
+
+  for (size_t i = 0; i < 10; ++i) {
+    uint32_t day_entry = (*get_entry<categorical_t>(data, i));
+
+    uint32_t hour_entry = (*get_entry<categorical_t>(data2, i));
+
+    uint32_t time_entry = (*get_entry<temporal_t>(data3, i));
+    uint32_t time_value = static_cast<temporal_t>(time_entry / static_cast<float>
+                          (604800)) * 604800;
+
+    coordinates_t coordinates_entry = (*get_entry<coordinates_t>(data4, i));
+
+    std::cout << day_entry << "," << hour_entry << "," << time_value << "," <<
+              coordinates_entry.lat << "," << coordinates_entry.lon << std::endl;
   }
 
   return 0;
