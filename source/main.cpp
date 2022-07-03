77f732d91d33c90148485da86888b014e2cbc340
Mateo Gonzales Navarrete <mgonzalesn17@outlook.com>
2022-07-03 00:10:06 -0500

    Victoria data writer

--- source/main.cpp
+++ source/main.cpp
@@ -20,7 +20,7 @@ struct coordinates_t {
 BinaryHeader header;
 
 std::string path = "../data/brightkite.nds";
-std::string write_path = "../data/brightkite_with_classes_ni.nds";
+std::string write_path = "../data/victoria_with_classes.nds";
 
 template <typename T>
 void get_data(std::vector<uint8_t>& data, uint8_t offset) {
@@ -42,14 +42,21 @@ T* get_entry(std::vector<uint8_t>& data, size_t id) {
 }
 
 int main() {
-  std::ifstream infile("../data/brightkite_with_classes_ni.csv");
+  std::ifstream infile("../data/victoria_with_classes.csv");
+  // "ALCOHOLTIME", "POLICE_ATTEND", "SPEED_ZONE", "FATALITY", "SERIOUSINJURY", "OTHERINJURY",
+  // "NONINJURED", "MALES", "FEMALES", "LONGITUDE", "LATITUDE", "class"
 
-  std::vector<uint8_t> days;
-  std::vector<uint8_t> hrs;
-  std::vector<uint32_t> timestamps;
+  std::vector<uint8_t> alcoholtime;
+  std::vector<uint8_t> police_attend;
+  std::vector<uint8_t> speed_zone;
+  std::vector<uint8_t> fatality;
+  std::vector<uint8_t> seriousinjury;
+  std::vector<uint8_t> otherinjury;
+  std::vector<uint8_t> noninjured;
+  std::vector<uint8_t> males;
+  std::vector<uint8_t> females;
   std::vector<coordinates_t> coords;
   std::vector<uint8_t> classes;
-  std::vector<float> color_classes;
 
   std::string line, word;
 
@@ -60,125 +67,67 @@ int main() {
     // std::cout << str.str() << std::endl;
 
     getline(str, word, ',');
-    days.push_back(std::stoi(word));
+    alcoholtime.push_back(std::stoi(word));
 
     getline(str, word, ',');
-    hrs.push_back(std::stoi(word));
+    police_attend.push_back(std::stoi(word));
 
     getline(str, word, ',');
-    timestamps.push_back(std::stoi(word));
+    speed_zone.push_back(std::stoi(word));
 
     getline(str, word, ',');
-    float lat = std::stof(word);
+    fatality.push_back(std::stoi(word));
+
+    getline(str, word, ',');
+    seriousinjury.push_back(std::stoi(word));
+
+    getline(str, word, ',');
+    otherinjury.push_back(std::stoi(word));
+
+    getline(str, word, ',');
+    noninjured.push_back(std::stoi(word));
+
+    getline(str, word, ',');
+    males.push_back(std::stoi(word));
+
+    getline(str, word, ',');
+    females.push_back(std::stoi(word));
+
     getline(str, word, ',');
     float lon = std::stof(word);
+    getline(str, word, ',');
+    float lat = std::stof(word);
     coords.push_back(coordinates_t(lat, lon));
 
     getline(str, word, ',');
     classes.push_back(std::stoi(word));
-
-    getline(str, word);
-    color_classes.push_back(std::stof(word));
   }
 
   for (size_t i = 0; i < 10; ++i)
-    std::cout << +days[i] << " " << +hrs[i] << " " << timestamps[i] << " " <<
-              coords[i].lat << " " << coords[i].lon << " " << +classes[i] << " " <<
-              color_classes[i] << "\n";
+    std::cout << +alcoholtime[i] << " " << +police_attend[i] << " " << +speed_zone[i] << " " <<
+              +fatality[i] << " " << +seriousinjury[i] << " " << +otherinjury[i] << " " <<
+              +noninjured[i] << " " << +males[i] << " " << +females[i] << " " <<
+              coords[i].lat << " " << coords[i].lon << " " << +classes[i] << "\n";
 
-  std::ofstream outFile("../data/brightkite_with_classes_ni.nds",
+  std::ofstream outFile("../data/victoria_with_classes.nds",
                         std::ios::binary);
 
-  std::ifstream infile3("../data/brightkite.nds", std::ios::binary);
-
-  infile3.read((char*)&header, sizeof(BinaryHeader));
-
-  infile3.close();
+  header.bytes = 0;
+  header.records = 19726;
 
   outFile.write((char*)&header, sizeof(BinaryHeader));
-  outFile.write((char*)&coords[0], sizeof(coordinates_t) * days.size());
-  outFile.write((char*)&days[0], sizeof(uint8_t) * days.size());
-  outFile.write((char*)&hrs[0], sizeof(uint8_t) * days.size());
-  outFile.write((char*)&timestamps[0], sizeof(uint32_t) * days.size());
-  outFile.write((char*)(&classes[0]), sizeof(uint8_t) * days.size());
-  outFile.write((char*)(&color_classes[0]), sizeof(float) * days.size());
+  outFile.write((char*)&coords[0], sizeof(coordinates_t) * alcoholtime.size());
+  outFile.write((char*)&alcoholtime[0], sizeof(uint8_t) * alcoholtime.size());
+  outFile.write((char*)&police_attend[0], sizeof(uint8_t) * alcoholtime.size());
+  outFile.write((char*)&speed_zone[0], sizeof(uint8_t) * alcoholtime.size());
+  outFile.write((char*)&fatality[0], sizeof(uint8_t) * alcoholtime.size());
+  outFile.write((char*)&seriousinjury[0], sizeof(uint8_t) * alcoholtime.size());
+  outFile.write((char*)&otherinjury[0], sizeof(uint8_t) * alcoholtime.size());
+  outFile.write((char*)&noninjured[0], sizeof(uint8_t) * alcoholtime.size());
+  outFile.write((char*)&males[0], sizeof(uint8_t) * alcoholtime.size());
+  outFile.write((char*)&females[0], sizeof(uint8_t) * alcoholtime.size());
+  outFile.write((char*)&classes[0], sizeof(uint8_t) * alcoholtime.size());
   outFile.close();
-
-  std::cout << "Reading..." << std::endl;
-
-  std::ifstream infile2("../data/brightkite_with_classes_ni.nds",
-                        std::ios::binary);
-
-  infile2.read((char*)&header, sizeof(BinaryHeader));
-
-  infile2.close();
-
-  std::cout << "Total number of records: " << header.records << "\n";
-
-  // Read Dimensions
-  std::vector<uint8_t> data;
-
-  // Read day_of_week
-  typedef uint8_t categorical_t;
-  uint8_t offset = 8;
-  get_data<categorical_t>(data, offset);
-
-  // for (size_t i = 0; i < header.records; ++i) {
-  // uint32_t entry = (*get_entry<categorical_t>(data, i));
-  // std::cout << entry << ";";
-  // }
-  // std::cout << std::endl;
-
-
-  // Read hour_of_day
-  std::vector<uint8_t> data2;
-  typedef uint8_t categorical_t;
-  offset = 9;
-  get_data<categorical_t>(data2, offset);
-
-  // for (size_t i = 0; i < header.records; ++i) {
-  // uint32_t entry = (*get_entry<categorical_t>(data2, i));
-  // std::cout << entry << ";";
-  // }
-  // std::cout << std::endl;
-
-
-  // Read temporal
-  std::vector<uint8_t> data3;
-  typedef uint32_t temporal_t;
-  offset = 10;
-  get_data<temporal_t>(data3, offset);
-
-  // for (size_t i = 0; i < header.records; ++i) {
-  // uint32_t entry = (*get_entry<temporal_t>(data3, i));
-  // uint32_t value = static_cast<temporal_t>(entry / static_cast<float>(604800)) * 604800;
-  // std::cout << value << ";";
-  // }
-
-  // Read spatial
-  std::vector<uint8_t> data4;
-  offset = 0;
-  get_data<coordinates_t>(data4, offset);
-
-  // for (size_t i = 0; i < header.records; ++i) {
-  // coordinates_t entry = (*get_entry<coordinates_t>(data4, i));
-  // std::cout << entry.lat << std::endl;
-  // }
-
-  for (size_t i = 0; i < 10; ++i) {
-    uint32_t day_entry = (*get_entry<categorical_t>(data, i));
-
-    uint32_t hour_entry = (*get_entry<categorical_t>(data2, i));
-
-    uint32_t time_entry = (*get_entry<temporal_t>(data3, i));
-    uint32_t time_value = static_cast<temporal_t>(time_entry / static_cast<float>
-                          (604800)) * 604800;
-
-    coordinates_t coordinates_entry = (*get_entry<coordinates_t>(data4, i));
-
-    std::cout << day_entry << "," << hour_entry << "," << time_value << "," <<
-              coordinates_entry.lat << "," << coordinates_entry.lon << std::endl;
-  }
 
   return 0;
 }
